f5a23600c3ecd01266e213d58369395ec64f5860
Rajashekar <kamerarajashekar@gmail.com>
2026-07-01 08:40:43 +0000

    Initial Spring Boot project

---
+++ src/test/java/com/raja/java_demo/JavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raja.java_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
